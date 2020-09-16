b4eb1dbb0ce572b66c448f7f364178697d6e7c8a
Nicholas Keller <nicholas_keller@icloud.com>
2020-09-16 08:05:46 -0500

    (P) WPF - Classes

---
+++ (P) WPF - Classes/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Submit_Click(object sender, RoutedEventArgs e)
+        {
+            EntryForm Applicant = new EntryForm(txtbx_Name.Text, txtbx_Address.Text, Convert.ToInt32(txtbx_Zipcode.Text));
+            lbx_ApplicantInfo.Items.Add(Applicant.ToString());
+        }
+    }
+}
