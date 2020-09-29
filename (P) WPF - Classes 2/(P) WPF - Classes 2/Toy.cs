fd95b84d8c8d37697fee87a6dc6201c1c9ccb6c2
Nicholas Keller <nicholas_keller@icloud.com>
2020-09-29 14:36:06 -0500

    sync

---
+++ (P) WPF - Classes 2/(P) WPF - Classes 2/Toy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _P__WPF___Classes_2
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        private string Aisle { get; }
+
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = -1;
+            Aisle = string.Empty;
+        }
+
+        public Toy( string Manufacturer, string Name, double Price)
+        {
+            this.Manufacturer = Manufacturer;
+            this.Name = Name;
+            this.Price = Price;
+        }
+
+        public string GetAisle()
+        {
+            string Aisle = $"{Manufacturer[0].ToString().ToUpper()}{Price.ToString()}";
+            return Aisle;
+        }
+
+        public override string ToString()
+        {
+            string Formatted = $"{{{Manufacturer}}}-{{{Name}}}";
+            return Formatted;
+        }
+
+
+    }
+}
